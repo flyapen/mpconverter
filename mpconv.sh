918632ff6627be0b11d2afb0ab669fcbe00487ef
Christoph E. Driessen <christoph@entwinemedia.com>
2012-11-22 10:52:10 +0100

    fixed start script

--- mpconv.sh
+++ mpconv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
-java -jar mpconverter_2.9.2-1.0-one-jar.jar $1 2>/dev/null
+java -jar $SCRIPTPATH/mpconverter_2.9.2-1.0-one-jar.jar $1 2>/dev/null
